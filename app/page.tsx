cbf1c5ef6fdd8b755fd7061c3376912ffaf5ffdc
Takubo Tomoya <tkb1003@gmail.com>
2025-05-28 23:44:13 +0900

    タイトルを中央寄せして、大きさなどを修正

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,13 @@ export default function Home() {
   return (
     <>
       <div className="min-h-screen flex items-center justify-center bg-gradient-to-br front-pupple200 from-purple-100 to-pink-100">
-        <h1>🔢カウンターアプリ</h1>
+        <div>
+          <div className="text-center mb-8">
+            <div className="text-center mb-8">
+              <h1 className="text-3xl font-bold text-gray-800">🔢カウンターアプリ</h1>
+            </div>
+          </div>
+        </div>
         <div>0</div>
         <button>-</button>
         <button>+</button>
