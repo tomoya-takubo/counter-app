92907e62c38731519c5f1141a4606299d1958cd9
Takubo Tomoya <tkb1003@gmail.com>
2025-05-28 23:37:42 +0900

    背景色を付けた

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <>
-      <div className="min-h-screen flex items-center justify-center">
+      <div className="min-h-screen flex items-center justify-center bg-gradient-to-br front-pupple200 from-purple-100 to-pink-100">
         <h1>🔢カウンターアプリ</h1>
         <div>0</div>
         <button>-</button>
