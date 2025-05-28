11b3eec65217ebf940e1b1b6537529dfef302e48
Takubo Tomoya <tkb1003@gmail.com>
2025-05-28 23:54:29 +0900

    背景色を追加。タイトルとカウントを縦並日に

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,19 @@
 export default function Home() {
   return (
     <>
-      <div className="min-h-screen flex items-center justify-center bg-gradient-to-br front-pupple200 from-purple-100 to-pink-100">
+      <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-purple-100 to-pink-100">
         <div>
           <div className="text-center mb-8">
             <div className="text-center mb-8">
               <h1 className="text-3xl font-bold text-gray-800">🔢カウンターアプリ</h1>
             </div>
+            <div className="text-center mb-8">
+              <div className="text-5xl font-bold text-gray-800">0</div>
+            </div>
+            <button>-</button>
+            <button>+</button>
           </div>
         </div>
-        <div>0</div>
-        <button>-</button>
-        <button>+</button>
       </div>
     </>
   );
