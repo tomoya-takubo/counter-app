4c3ec939bfa377014a172bf25718fdd71ce4c983
Takubo Tomoya <tkb1003@gmail.com>
2025-05-28 23:31:24 +0900

    ゼ㎜田尾wp画面中央に配置

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <>
-      <div>
+      <div className="min-h-screen flex items-center justify-center">
         <h1>🔢カウンターアプリ</h1>
         <div>0</div>
         <button>-</button>
